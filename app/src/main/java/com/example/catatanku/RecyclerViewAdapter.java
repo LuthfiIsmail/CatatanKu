d0762ff761951378fbee9aa751ced59e214ca995
LuthfiIsmail <luthfiismailv.siswa@smkm4bdg.sch.id>
2021-02-26 00:04:08 +0700

    Commit Pertama

---
+++ app/src/main/java/com/example/catatanku/RecyclerViewAdapter.java
@@ -0,0 +1,40 @@
+package com.example.catatanku;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class RecyclerViewAdapter extends RecyclerView.Adapter<NoteViewHolder> {
+    List<Note> noteList;
+
+    public RecyclerViewAdapter(List<Note> noteList) {
+        this.noteList = noteList;
+    }
+
+    @NonNull
+    @Override
+    public NoteViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater layoutInflater = LayoutInflater.from(parent.getContext());
+        View view = layoutInflater.inflate(R.layout.row_item, parent, false);
+        NoteViewHolder noteViewHolder = new NoteViewHolder(view);
+
+        return noteViewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull NoteViewHolder holder, int position) {
+        holder.txtTanggal.setText(noteList.get(position).getTanggal());
+        holder.txtJudul.setText(noteList.get(position).getJudul());
+        holder.txtDeskripsi.setText(noteList.get(position).getDeskripsi());
+    }
+
+    @Override
+    public int getItemCount() {
+        return noteList.size();
+    }
+}
